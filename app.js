d02f883e08c837bf60b6635564494e68022b4d7a
david <morenza@gmail.com>
2016-01-13 23:21:11 +0000

    Added scheduler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var RouteDynDns = require('./routeDynDns');
+var schedule = require('node-schedule');
 
 var awsAccessKey = "the one without slash /";
 var awsSecret = "this thing/another thing";
@@ -7,4 +8,10 @@ var hostedZoneId = "another ID";
 var domainsToChange = ['videos.morenware.com.'];
 var dynDns = new RouteDynDns(awsAccessKey, awsSecret, domainsToChange, hostedZoneId);
 
-dynDns.updateRecords();
+var j = schedule.scheduleJob('* */6 * * *', function(){
+  dynDns.updateRecords();
+});
+
+if (typeof j !== undefined) {
+  console.log("Scheduled job", j);
+}
